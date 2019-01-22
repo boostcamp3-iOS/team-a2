89ec8897812e664731e3cf7d4eaacac8656c9018
Caution Kim <caution.dev@gmail.com>
2019-01-22 14:02:54 +0900

    initial project

---
+++ OneDayProto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OneDayProto
+//
+//  Created by juhee on 22/01/2019.
+//  Copyright © 2019 teamA2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
