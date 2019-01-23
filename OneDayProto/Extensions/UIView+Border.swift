92c81c9857eebc883674bce641e1923f13a2f746
Caution Kim <caution.dev@gmail.com>
2019-01-23 10:41:38 +0900

    [수정] 프로젝트 아키텍쳐 추가
    - AppDelegate
    - Views : Custom View
    - Resources : 애매한 애들, 에셋
    - Controllers : ViewControllers
    - CoreData : CoreData 사용에 필요한 파일들
    - Model : Model 파일 (임시파일)
    - Extensions : Extension 파일
    	 * Naming Rule : ExtensionTarget+Content
    	 * ex) UIView+Border.swift

---
+++ OneDayProto/Extensions/UIView+Border.swift
@@ -0,0 +1,30 @@
+//
+//  UIView+Border.swift
+//  OneDayProto
+//
+//  Created by juhee on 23/01/2019.
+//  Copyright © 2019 teamA2. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+}
