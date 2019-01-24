5fac239858b8749dccd8ad4a14c2689638dea096
mohok <pnujhseo@pusan.ac.kr>
2019-01-24 16:01:30 +0900

    [생성] 사이드 메뉴, 필터 뷰 컨트롤러 생성
    - FilterViewController 추가
    - SideMenuViewController 추가
    - TestViewController 추가
    - 뷰컨트롤러에 관련된 테이블 셀 추가

---
+++ OneDayProto/Views/SideMenuCell/SideMenuEditCell.swift
@@ -0,0 +1,38 @@
+//
+//  SideMenuEditCell.swift
+//  OneDayProto
+//
+//  Created by 정화 on 23/01/2019.
+//  Copyright © 2019 teamA2. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuEditCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupCellView()
+    }
+
+    let editTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = ""
+        label.backgroundColor = .white
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension SideMenuEditCell {
+    func setupCellView() {
+        addSubview(editTitleLabel)
+        editTitleLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 16).isActive = true
+        editTitleLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+
+    }
+}
---
+++ OneDayProto/Views/SideMenuCell/SideMenuJournalAddCell.swift
@@ -0,0 +1,53 @@
+//
+//  SideMenuJournalAddCell.swift
+//  OneDayProto
+//
+//  Created by 정화 on 23/01/2019.
+//  Copyright © 2019 teamA2. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuJournalAddCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupCellView()
+    }
+
+    let borderView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.borderWidth = 1
+        view.layer.cornerRadius = 5
+        view.layer.borderColor = UIColor.gray.cgColor
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    let addLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Start a New Journal!"
+        label.backgroundColor = .white
+        label.textColor = .doBlue
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension SideMenuJournalAddCell {
+    func setupCellView() {
+        addSubview(borderView)
+        borderView.leftAnchor.constraint(equalTo: leftAnchor, constant: 8).isActive = true
+        borderView.topAnchor.constraint(equalTo: topAnchor, constant: 8).isActive = true
+        borderView.rightAnchor.constraint(equalTo: rightAnchor, constant: -8).isActive = true
+        borderView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8).isActive = true
+
+        addSubview(addLabel)
+        addLabel.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        addLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+    }
+}
