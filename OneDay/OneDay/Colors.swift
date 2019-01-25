96083b4260ebdf594345aad222075a8d5153e5d3
mohok <pnujhseo@pusan.ac.kr>
2019-01-25 16:24:39 +0900

    [생성] 사이드, 필터 뷰 컨트롤러 생성
    - 키워드 검색 기능 포함
    
    * 이전 프로젝트와 클론

---
+++ OneDay/OneDay/Colors.swift
@@ -0,0 +1,27 @@
+//
+//  Colors.swift
+//  OneDay
+//
+//  Created by juhee on 23/01/2019.
+//  Copyright © 2019 teamA2. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static var doBlue: UIColor {
+        return UIColor(named: "doBlue")!
+    }
+    static var doDark: UIColor {
+        return UIColor(named: "doDark")!
+    }
+    static var doGray: UIColor {
+        return UIColor(named: "doGray")!
+    }
+    
+    static var doLight: UIColor {
+        return UIColor(named: "doLight")!
+    }
+    
+}
---
+++ OneDay/OneDay/Views/FilterCell/SearchedKeywordCell.swift
@@ -0,0 +1,64 @@
+//
+//  SearchedKeywordCell.swift
+//  OneDay
+//
+//  Created by 정화 on 23/01/2019.
+//  Copyright © 2019 teamA2. All rights reserved.
+//
+
+import UIKit
+//Keywords - 돋보기 + "문자열" + 일치하는 엔트리 수를 보여주는 셀
+class SearchedKeywordCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupCell()
+    }
+    
+    let searchIcon: UIImageView = {
+        var imageView = UIImageView()
+        imageView.image = UIImage(named: "keywordsSearch") ?? UIImage()
+        imageView.contentMode = .scaleAspectFit
+        imageView.contentMode = .center
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = ""
+        label.textColor = .doBlue
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let countLabel: UILabel = {
+        let label = UILabel()
+        label.text = "0"
+        label.textColor = .doBlue
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension SearchedKeywordCell {
+    func setupCell() {
+        addSubview(searchIcon)
+        searchIcon.leftAnchor.constraint(equalTo: leftAnchor, constant: 16).isActive = true
+        searchIcon.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        searchIcon.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        addSubview(titleLabel)
+        titleLabel.leftAnchor.constraint(equalTo: searchIcon.rightAnchor, constant: 16).isActive = true
+        titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        addSubview(countLabel)
+        countLabel.leftAnchor.constraint(equalTo: titleLabel.rightAnchor, constant: 16).isActive = true
+        countLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        countLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: -24).isActive = true
+        
+    }
+}
