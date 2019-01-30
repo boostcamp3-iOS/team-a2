49f1e35abb2dc7ebed80e8411e52efb8eb84ff22
Caution Kim <caution.dev@gmail.com>
2019-01-30 14:14:30 +0900

    [생성] 텍스트 뷰에서 텍스트 이미지 Drag Drop 기능 추가
    - UIImagePickerController 사용해서 이미지 불러오기
    - 미구현 : 드래그 드랍시 Preview 생성

--- OneDay_Prototype/OneDayProto/Controller/EntryViewController.swift
+++ OneDay_Prototype/OneDayProto/Controller/EntryViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 import CoreData
+import MobileCoreServices
 
 class EntryViewController: UIViewController {
 
     @IBOutlet weak var dateLabel: UILabel!
-    @IBOutlet weak var contentField: UITextView!
+    @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var timeLabel: UILabel!
 
     var entry: EntryVO?
@@ -24,18 +25,24 @@ class EntryViewController: UIViewController {
         var dateSet: DateStringSet = DateStringSet(date: Date())
         if let data = entry {
             dateSet = DateStringSet(date: data.date)
-//            contentField.text = data.contents.reduce("") { (result, contents) -> String in
-//                return "\(result)\(contents.content)"
-//            }
-            contentField.text = data.title
+            textView.text = data.title
         }
 
         dateLabel.text = dateSet.full
         timeLabel.text = dateSet.time
+        textView.textDragDelegate = self
+    }
+
+    @IBAction func showPhoto(_ sender: UIButton) {
+        let pickerViewController = UIImagePickerController()
+        pickerViewController.delegate = self
+        pickerViewController.sourceType = .photoLibrary
+        pickerViewController.allowsEditing = true
+        present(pickerViewController, animated: true, completion: nil)
     }
 
     @IBAction func saveAndDismiss(_ sender: UIButton) {
-        guard let content = contentField.text else {
+        guard let content = textView.text else {
             return
         }
         if let entry = entry {
@@ -51,4 +58,64 @@ class EntryViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
 
+}
+
+extension EntryViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+        insertAtTextViewCursor(attributedString: createAttributedString(with: image))
+        picker.dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func tapAndHideKeyboard(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+
+    fileprivate func createAttributedString(with image: UIImage) -> NSAttributedString {
+        let textAttachment = NSTextAttachment()
+        textAttachment.image = image
+        let oldWidth = textAttachment.image!.size.width
+        let scaleFactor = oldWidth / (textView.frame.size.width - 10)
+        textAttachment.image = UIImage(cgImage: textAttachment.image!.cgImage!, scale: scaleFactor, orientation: .up)
+        let attrStringWithImage = NSAttributedString(attachment: textAttachment)
+        return attrStringWithImage
+    }
+
+    fileprivate func insertAtTextViewCursor(attributedString: NSAttributedString) {
+        guard let selectedRange = textView.selectedTextRange else {
+            return
+        }
+
+        // If here, insert <attributedString> at cursor
+        let cursorIndex = textView.offset(from: textView.beginningOfDocument, to: selectedRange.start)
+        let mutableAttributedText = NSMutableAttributedString(attributedString: textView.attributedText)
+        mutableAttributedText.insert(attributedString, at: cursorIndex)
+        textView.attributedText = mutableAttributedText
+    }
+}
+
+extension EntryViewController: UITextDragDelegate, UITextDropDelegate {
+
+    func textDraggableView(_ textDraggableView: UIView & UITextDraggable, itemsForDrag dragRequest: UITextDragRequest) -> [UIDragItem] {
+        if let string = textView.text(in: dragRequest.dragRange) {
+            let itemProvider = NSItemProvider(object: string as NSString)
+            let item = UIDragItem(itemProvider: itemProvider)
+            item.localObject = true
+            return [item]
+        } else {
+            return []
+        }
+    }
+    func textDraggableView(_ textDraggableView: UIView & UITextDraggable, dragPreviewForLiftingItem item: UIDragItem, session: UIDragSession) -> UITargetedDragPreview? {
+
+        let center = CGPoint(x: textView.bounds.midX, y: textView.bounds.midY)
+        let target = UIDragPreviewTarget(container: textView, center: center)
+        return UITargetedDragPreview(view: textDraggableView.subviews[1], parameters: UIDragPreviewParameters(), target: target)
+
+    }
 }
