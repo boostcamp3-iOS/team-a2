7ef7818aca5d07df01f7c2b62e7d5dbbdfb42c11
Caution Kim <caution.dev@gmail.com>
2019-02-11 11:46:30 +0900

    [생성] Photos 탭 화면 구성
    파일추가
    - Photos.storyboard > Main.storyboard 에서 연결
    - PhotosCollectionViewCell.swift
    - 임시 image 추가
    
    사진 있는 엔티티 목록 데이터 불러오기, 화면 구성 #21

--- OneDay/OneDay/Controller/PhotosViewController.swift
+++ OneDay/OneDay/Controller/PhotosViewController.swift
@@ -9,22 +9,44 @@
 import UIKit
 
 class PhotosViewController: UIViewController {
+    
+    @IBOutlet weak var photoCollectionView: UICollectionView!
+    
+    var photos: [String] = ["image","image","image","image","image","image","image","image","image","image","image","image","image"]
+    private let reuseIdentifier = "photo_cell"
+    fileprivate let itemsPerRow: CGFloat = 3
+    fileprivate let sectionInsets = UIEdgeInsets(top: 2.0, left: 4.0, bottom: 2.0, right: 2.0)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setCollectionViewLayout()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setCollectionViewLayout() {
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        let insets = UIEdgeInsets(top: 1, left: 1, bottom: 1, right: 1)
+        let itemsPerRow: CGFloat = 3
+        let availableWidth = (view.frame.width - (insets.left * itemsPerRow)) / 3
+        layout.sectionInset = UIEdgeInsets(top: 1, left: 1, bottom: 1, right: 1)
+        layout.itemSize = CGSize(width: availableWidth, height: availableWidth)
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
+        photoCollectionView.collectionViewLayout = layout
     }
-    */
 
+}
+
+extension PhotosViewController : UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? PhotosCollectionViewCell else {
+            preconditionFailure("No expected cell type casting by PhotosCollectionViewCell")
+        }
+        cell.dayLabel.text = "12"
+        return cell
+    }
 }
---
+++ OneDay/OneDay/Views/Photos/PhotosCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  PhotosCollectionViewCell.swift
+//  OneDay
+//
+//  Created by juhee on 11/02/2019.
+//  Copyright © 2019 teamA2. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var monthAndYearLabel: UILabel!
+    
+    
+    
+}
