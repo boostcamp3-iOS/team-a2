2822fe9696fd7e209b042c406c33d4adf055883b
mohok <pnujhseo@pusan.ac.kr>
2019-02-23 14:22:06 +0900

    [수정] 맵 중앙 좌표 코드 수정

--- OneDay/OneDay/Controller/CollectedEntriesViewController.swift
+++ OneDay/OneDay/Controller/CollectedEntriesViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import CoreData
+import CoreLocation
+import GLKit
 import MapKit
 import UIKit
 
@@ -48,9 +50,11 @@ class CollectedEntriesViewController: UIViewController {
     }()
     
     var entriesData = [Entry]()
-    private var shouldPresentMapView = false // 엔트리에 위치 정보가 있을 때만 맵 뷰를 보여줍니다.
+    /// 엔트리에 위치 정보가 있을 때만 맵 뷰를 보여줍니다.
+    private var shouldPresentMapView = false
     private let headerMapView = CollectedEntriesHeaderMapView()
-    
+    private var entriesLocation: [MapPinLocation] = []
+
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
@@ -69,17 +73,13 @@ class CollectedEntriesViewController: UIViewController {
     @objc func dismissCollectedEntriesView() {
         dismiss(animated: false, completion: nil)
     }
-    
-    private var coordinates: [MapPinLocation] = []
-    private var latitude = [CLLocationDegrees]()
-    private var longitude = [CLLocationDegrees]()
 
-    private func addLocationToMapView() { // 맵 뷰 어노테이션에 사용하기 위해 좌표를 배열에 담는다
+    /// 맵 어노테이션에 사용하기 위해 좌표를 배열에 추가합니다.
+    private func addLocationToMapView() {
         entriesData.forEach { entry in
             if let location = entry.location {
                 shouldPresentMapView = true
-                
-                coordinates.append(
+                entriesLocation.append(
                     MapPinLocation(
                         coordinate: CLLocationCoordinate2D(
                             latitude: location.latitude,
@@ -89,7 +89,27 @@ class CollectedEntriesViewController: UIViewController {
     }
 }
 
-extension CollectedEntriesViewController: UITableViewDelegate, UITableViewDataSource {
+// MARK: - UITableViewDelegate
+
+extension CollectedEntriesViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if shouldPresentMapView {
+            let mapViewHeight = UIScreen.main.bounds.height*0.4
+            return mapViewHeight
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        headerMapView.bind(locations: entriesLocation)
+        return headerMapView
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension CollectedEntriesViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return entriesData.count
     }
@@ -100,135 +120,16 @@ extension CollectedEntriesViewController: UITableViewDelegate, UITableViewDataSo
         ) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(
             withIdentifier: "listCellId",
-            for: indexPath)
-            as? CollectedEntriesListCell
-        else {
-            preconditionFailure("CollectedEntriesListCell Error")
-        }
+            for: indexPath) as? CollectedEntriesListCell
+        else { preconditionFailure("Error") }
         
         let entry = entriesData[indexPath.row]
-        let attributedText = NSMutableAttributedString()
-        setupAttributedText(about: entry, text: attributedText)
-        resizeImageAttachment(in: attributedText)
-
-        cell.contentsListTextView.attributedText = attributedText
+        cell.bind(entry: entry)
         return cell
-        }
-    
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        let mapViewHeight = UIScreen.main.bounds.height*0.4
-        if shouldPresentMapView {
-            return mapViewHeight
-        } else {
-            return 0
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let initialLocation = averageLocationOfCoordinates()
-        var regionRadiusOnMap = mapViewPresentingRange(from: initialLocation, scale: 0.5)
-        if regionRadiusOnMap < 5000 {
-            regionRadiusOnMap = 5000 // 최소 5km 반경까지 지도가 그려짐
-        }
-        headerMapView.bind(
-            coordinates: coordinates,
-            initialLocation: initialLocation,
-            regionRadius: regionRadiusOnMap)
-        return headerMapView
     }
 }
 
 extension CollectedEntriesViewController {
-    
-    private func setupAttributedText(
-        about entry: Entry,
-        text attributedText: NSMutableAttributedString) {
-        appendContents(entry, attributedText)
-        appendJournalTitle(entry, attributedText)
-        appendDate(attributedText, entry)
-        apptendAddress(entry, attributedText)
-        apptendTemperature(entry, attributedText)
-    }
-    
-    private func appendContents(_ entry: Entry, _ attributedText: NSMutableAttributedString) {
-        if let contents = entry.contents {
-            attributedText.append(NSMutableAttributedString(attributedString: contents))
-            attributedText.append(NSMutableAttributedString(string: "\n\n"))
-        }
-    }
-    
-    private func appendJournalTitle(_ entry: Entry, _ attributedText: NSMutableAttributedString) {
-        if let title = entry.journal?.title {
-            attributedText.append(NSMutableAttributedString(string: title, attributes:[
-                NSAttributedString.Key.foregroundColor : UIColor.doBlue,
-                NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 12)]))
-        }
-    }
-    
-    private func appendDate(_ attributedText: NSMutableAttributedString, _ entry: Entry) {
-        let formatter = DateFormatter.defualtInstance
-        formatter.locale = Locale(identifier: "ko_KR")
-        formatter.dateFormat = "YYYY년 MM월 dd일 EEEE a hh:mm"
-        
-        appendDot(at: attributedText)
-        attributedText.append(NSMutableAttributedString(string: formatter.string(from: entry.date)))
-    }
-    
-    private func apptendAddress(_ entry: Entry, _ attributedText: NSMutableAttributedString) {
-        if let address = entry.location?.address {
-            appendDot(at: attributedText)
-            attributedText.append(NSMutableAttributedString(string: address))
-        }
-    }
-    
-    private func apptendTemperature(_ entry: Entry, _ attributedText: NSMutableAttributedString) {
-        if let temparature = entry.weather?.tempature, let type = entry.weather?.type {
-            appendDot(at: attributedText)
-            attributedText.append(NSMutableAttributedString(string: "\(temparature) °C  "))
-            if let weatherType = WeatherType(rawValue: type) {
-                attributedText.append(NSMutableAttributedString(string: weatherType.summary))
-            }
-        }
-    }
-    /**
-    - 엔트리 콘텐츠 안에 있는 이미지를 리사이즈하여 다시 제 위치에 집어넣습니다.
-    - parameter attributedText: 
-     */
-    private func resizeImageAttachment(in attributedText: NSMutableAttributedString) {
-        attributedText.enumerateAttribute(
-            NSAttributedString.Key.attachment,
-            in: NSRange(location: 0, length: attributedText.length),
-            options: [],
-            using: { value, range, _ -> Void in
-                if value is NSTextAttachment {
-                    guard let attachment: NSTextAttachment = value as? NSTextAttachment
-                    else { return }
-                    
-                    if let oldImage = attachment.image {
-                        var newImage = UIImage()
-                        newImage = oldImage
-                        // 124: textview left+right margin in view
-                        let imageWidth = UIScreen.main.bounds.width - 124
-                        
-                        let newAttachment: NSTextAttachment = NSTextAttachment()
-                        newAttachment.image = newImage.resizeImageToFit(newWidth: imageWidth)
-                        attributedText.replaceCharacters(
-                            in: range,
-                            with: NSAttributedString(attachment: newAttachment))
-                    }
-                }
-        })
-    }
-    
-    // 엔트리 정보들을 구분하기 위해 텍스트에 · 을 추가합니다.
-    private func appendDot(at attributedString: NSMutableAttributedString) {
-        attributedString.append(
-            NSMutableAttributedString(
-                string: " · ",
-                attributes: [
-                    NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 18)]))
-    }
-    
     private func setupTableView() {
         view.addSubview(topFloatingView)
         topFloatingView.topAnchor.constraint(
@@ -241,7 +142,7 @@ extension CollectedEntriesViewController {
         dateLabel.leftAnchor.constraint(equalTo: topFloatingView.leftAnchor,
                                         constant: 24).isActive = true
         dateLabel.centerYAnchor.constraint(equalTo: topFloatingView.centerYAnchor).isActive = true
-
+        
         topFloatingView.addSubview(doneButton)
         doneButton.rightAnchor.constraint(equalTo: topFloatingView.rightAnchor,
                                           constant: -24).isActive = true
@@ -257,38 +158,5 @@ extension CollectedEntriesViewController {
         
         tableView.delegate = self
         tableView.dataSource = self
-    }
-}
-
-//FIXME: 함수 다시 확인하기
-extension CollectedEntriesViewController {
-    func averageLocationOfCoordinates() -> CLLocation {
-        coordinates.forEach { coordinates in
-            latitude.append(coordinates.coordinate.latitude)
-            longitude.append(coordinates.coordinate.longitude)
-        }
-
-        let averageLatitude = latitude.reduce(0, +) / Double(latitude.count)
-        let averagelongitude = longitude.reduce(0, +) / Double(longitude.count)
-//        let averageLocation = CLLocation(latitude: averageLatitude, longitude: averagelongitude)
-
-        let averageLocation = CLLocation(latitude: latitude.first ?? 0, longitude: longitude.first ?? 0)
-        return averageLocation
-    }
-    
-    func mapViewPresentingRange(from averageLocation: CLLocation, scale: Double) -> Double {
-        var distanceFromAverageLocation = [Double]()
-        for index in 0..<latitude.count {
-            distanceFromAverageLocation.append(
-                averageLocation.distance(
-                    from: CLLocation(
-                        latitude: latitude[index],
-                        longitude: longitude[index])))
-        }
-        guard let mapPresentingRange = distanceFromAverageLocation.max()
-        else {
-            preconditionFailure("Error")
-        }
-        return mapPresentingRange*scale
     }
 }
--- OneDay/OneDay/Views/CollectedEntries/CollectedEntriesHeaderMapView.swift
+++ OneDay/OneDay/Views/CollectedEntries/CollectedEntriesHeaderMapView.swift
@@ -18,53 +18,105 @@ class MapPinLocation: NSObject, MKAnnotation {
 }
   
 class CollectedEntriesHeaderMapView: MKMapView, MKMapViewDelegate {
+    func bind(locations: [MapPinLocation]) {
+        setCenter(of: locations)
+        addAnotation(on: locations)
+    }
+    
+// MARK: - setCenter(of: )
+    
+    /**
+     맵뷰의 중앙 좌표와 범위를 설정합니다.
+     - Parameter locations: 엔트리 정보에 포함된 지도의 좌표 배열입니다.
+     - 중앙 좌표를 알기 위해서 위도와 경도의 최댓값과 최솟값을 구해 각각의 중앙 값을 구합니다.
+     지도의 좌표 체계는 일반적으로 사용하는 카타시안 좌표계나, 극 좌표계와 다르기 때문에 위와 같은 방법으로 계산할 경우
+     중앙 값이 올바른지 확인하는 과정이 필요합니다. validateLogitude(_:,and:,for:) 메소드를 사용하여 확인하십시오.
+     */
+    private func setCenter(of locations: [MapPinLocation]) {
+        var maxLatitude: CLLocationDegrees = -90.0
+        var minLatitude: CLLocationDegrees = 90.0
+        var maxLongitude: CLLocationDegrees = -180.0
+        var minLongitude: CLLocationDegrees = 180.0
+        
+        locations.forEach { (entry) in
+            let latitude = entry.coordinate.latitude
+            let longitude = entry.coordinate.longitude
+            
+            minLatitude = min(latitude, minLatitude)
+            maxLatitude = max(latitude, maxLatitude)
+            minLongitude = min(longitude, minLongitude)
+            maxLongitude = max(longitude, maxLongitude)
+        }
+        
+        let centerLatitude = (maxLatitude + minLatitude)*0.5
+        var centerLongitude = (maxLongitude + minLongitude)*0.5
+        validateLogitude(maxLongitude, and: minLongitude, for: &centerLongitude)
+        let center = CLLocationCoordinate2DMake(centerLatitude, centerLongitude)
 
-    private var coordinates: [MapPinLocation] = []
+        let latitudeSpan = min((maxLatitude - minLatitude)*1.5, 180)
+        var longitudeSpan = min((maxLongitude - minLongitude)*1.5, 180)
+        optimizeMapView(by: &longitudeSpan)
+        let span = MKCoordinateSpan(latitudeDelta: latitudeSpan, longitudeDelta: longitudeSpan)
+        
+        let coordinateRegion = MKCoordinateRegion(center: center, span: span)
+        setRegion(coordinateRegion, animated: false)
+    }
     
-    private var initialLocation = CLLocation(latitude: 0, longitude: 0)
-    private var regionRadius: CLLocationDistance = 0 
+    /**
+     경도의 중앙값이 맞는지 확인하는 함수입니다. 본 함수는 충분히 검증되지 않았습니다. 예외 사항이 발생할 수 있음에 유의하십시오.
+     */
+    private func validateLogitude(
+        _ maxLongitude: CLLocationDegrees,
+        and minLongitude: CLLocationDegrees,
+        for centerLongitude: inout Double) {
+        if maxLongitude*minLongitude < 0 && abs(maxLongitude)+abs(minLongitude) > 180 {
+            centerLongitude = min(centerLongitude-180, centerLongitude)
+        }
+    }
+
+    /**
+     longitudeSpan의 값에 따라 지도 뷰를 최적화합니다.
+     - case 0:
+        엔트리 목록에 좌표가 하나만 있는 경우 latitudeSpan = 0, longitudeSpan = 0.15으로 바꿉니다.
+        경도 1도는 약 111km이므로 너비를 기준으로 약 1.7km에 해당하는 거리의 지도가 맵뷰에 표시됩니다.
+     - case 180:
+        longitudeSpan의 최댓값은 180으로 제한되어 있습니다. 이 값을 초과하는 경우 맵 뷰가 어노테이션이 있는 범위를
+        모두 보여주지 못할 수 있습니다. 사용자가 어노테이션을 찾을 수 있도록 유저 인터렉션을 허용합니다.
+     - parameter longitudeSpan: 지도를 보여 줄 범위에 사용하는 longitude delta 값입니다.
+     */
+    private func optimizeMapView(by longitudeSpan: inout Double) {
+        switch longitudeSpan {
+        case 0:
+            longitudeSpan = 0.015
+        case 180:
+            isZoomEnabled = false
+            isUserInteractionEnabled = true
+        default:
+            ()
+        }
+    }
     
-    func bind(coordinates: [MapPinLocation], initialLocation: CLLocation, regionRadius: CLLocationDistance) {
-        self.coordinates = coordinates
-        self.initialLocation = initialLocation
-        self.regionRadius = regionRadius
-        setCenterOfMap(on: initialLocation)
-        addAnotationOnMap(coordinates)
+// MARK: - addAnotation(on: )
+
+    private func addAnotation(on locations: [MapPinLocation]) {
+        for index in 0..<locations.count {
+            addAnnotation(locations[index])
+        }
     }
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         let annotationView = MKAnnotationView(
             annotation: annotation,
             reuseIdentifier: "collectedEntriesMapId")
-        if regionRadius < 5000000 {
-            configureAnnotation(at: annotationView, size: 20)
-            return annotationView
-        } else {
-            configureAnnotation(at: annotationView, size: 10)
-            return annotationView
-        }
+        configureAnnotation(at: annotationView, size: 20)
+        return annotationView
     }
-    
-    fileprivate func setCenterOfMap(on location: CLLocation) {
-        let coordinateRegion = MKCoordinateRegion(
-            center: location.coordinate,
-            latitudinalMeters: 3000,
-            longitudinalMeters: 3000)
-    
-        setRegion(coordinateRegion, animated: false)
-    }
-    
-    fileprivate func configureAnnotation(at annotationView: MKAnnotationView, size: CGFloat) {
-        annotationView.frame = CGRect(x: 0, y: 0, width: size, height: size)
-        annotationView.backgroundColor = .gray
+
+    private func configureAnnotation(at annotationView: MKAnnotationView, size: CGFloat) {
         annotationView.alpha = 0.5
+        annotationView.backgroundColor = .gray
         annotationView.layer.cornerRadius = size/2
-    }
-    
-    fileprivate func addAnotationOnMap(_ coordinates: [MapPinLocation]) {
-        for index in 0..<coordinates.count {
-            addAnnotation(coordinates[index])
-        }
+        annotationView.frame = CGRect(x: 0, y: 0, width: size, height: size)
     }
     
     override init(frame: CGRect = .zero) {
